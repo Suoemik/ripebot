f967051580338767169d1d8a72a8ae67c013fbfe
Skoroye <suoemikoroye@gmail.com>
2019-07-02 13:47:33 -0500

    Added processPostback

--- messageWebhook.js
+++ messageWebhook.js
@@ -1,10 +1,13 @@
 const processMessage = require("./processMessage");
+const processMessage = require("./processPostback");
 module.exports = (req, res) => {
   if (req.body.object === "page") {
     req.body.entry.forEach(entry => {
       entry.messaging.forEach(event => {
         if (event.message && event.message.text) {
           processMessage(event);
+        }else if (event.postback) {
+          processPostback(event);
         }else {
           console.log('received event', JSON.stringify(event));
         }
