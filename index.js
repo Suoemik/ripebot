430602f6da3ae5018724a342fd85405b183d2911
Skoroye <suoemikoroye@gmail.com>
2019-07-02 10:36:11 -0500

    Uncommented app.get("/"...

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.set('port', (process.env.PORT) || 3000)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(app.get('port'), () => console.log("Webhook server is listening, port", app.get('port')));
-// app.get('/', function (req, res) {
-//   res.send('hello world i am a chat bot')
-// })
+app.get('/', function (req, res) {
+   res.send('hello world i am a chat bot')
+ })
 const verificationController = require("./verification");
 const messageWebhookController = require("./messageWebhook");
 app.get("/", verificationController);
